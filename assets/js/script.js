e34dd7eb7d0deb1faebb0903c22952f760ff4c44
paulgibes <paulgibes@gmail.com>
2023-02-23 12:25:13 -0700

    pulled data from open weather and displayed current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// https: b18b2ada8a7cf01d1a6c89d2666509ec;
 var searchHistory = $("#searchHistory");
 var cityHistory = JSON.parse(localStorage.getItem("cities")) || [];
 localStorage.setItem("cities", JSON.stringify(cityHistory));
@@ -25,7 +23,11 @@ var seeHistory = function () {
   }
 };
 
-function getApi(cityName) {
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getCurrentWeather(cityName) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
@@ -37,6 +39,82 @@ function getApi(cityName) {
     })
     .then(function (data) {
       console.log(data);
+      var temp = data.main.temp;
+      var tempApparent = data.main.feels_like;
+      var tempMin = data.main.temp_min;
+      var tempMax = data.main.temp_max;
+      var tempRange = tempMin + "°F - " + tempMax + "°F";
+      var windSpeed = data.wind.speed;
+      var humidity = data.main.humidity;
+      var weather = data.weather[0].description;
+
+      $("#searchedCity").text(cityName);
+      $("#todaysWeather").append(
+        "<li class='card-text'>Temperature: " + temp + "°F</li>"
+      );
+      $("#todaysWeather").append(
+        "<li class='card-text'>Apparent Temperature: " +
+          tempApparent +
+          "°F</li>"
+      );
+      $("#todaysWeather").append(
+        "<li class='card-text'>Temperature Range: " + tempRange + "</li>"
+      );
+      $("#todaysWeather").append(
+        "<li class='card-text'>Wind Speed: " + windSpeed + " MPH</li>"
+      );
+      $("#todaysWeather").append(
+        "<li class='card-text'>Humidity: " + humidity + "%</li>"
+      );
+      $("#todaysWeather").append(
+        "<li class='card-text'>Weather: " + weather + "</li>"
+      );
+    });
+}
+
+function getForecastWeather(cityName) {
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&cnt=5&appid=b18b2ada8a7cf01d1a6c89d2666509ec&units=imperial";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (let i = 0; i < data.list.length; i++) {
+        var temp = data.list[i].main.temp;
+        var tempApparent = data.list[i].main.feels_like;
+        var tempMin = data.list[i].main.temp_min;
+        var tempMax = data.list[i].main.temp_max;
+        var tempRange = tempMin + "°F - " + tempMax + "°F";
+        var windSpeed = data.list[i].wind.speed;
+        var humidity = data.list[i].main.humidity;
+        var weather = data.list[i].weather[0].description;
+
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Temperature: " + temp + "°F</li>"
+        //   );
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Apparent Temperature: " +
+        //       tempApparent +
+        //       "°F</li>"
+        //   );
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Temperature Range: " + tempRange + "</li>"
+        //   );
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Wind Speed: " + windSpeed + " MPH</li>"
+        //   );
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Humidity: " + humidity + "%</li>"
+        //   );
+        //   $("#todaysWeather").append(
+        //     "<li class='card-text'>Weather: " + weather + "</li>"
+        //   );
+      }
     });
 }
 
@@ -49,13 +127,20 @@ $("#searchHistory").on("click", "#clearHistoryButton", function () {
 
 $("#searchForm").submit(function (event) {
   var userInput = inputEl.value;
+  console.log(userInput);
+  userInput = capitalize(userInput);
+  $("#currentWeather").css("display", "block");
+  $("#forecastWeather").css("display", "block");
+
   if (!($.inArray(userInput, cityHistory) > -1)) {
     cityHistory.unshift(userInput);
     localStorage.setItem("cities", JSON.stringify(cityHistory));
     seeHistory();
-    getApi(userInput);
+    getCurrentWeather(userInput);
+    getForecastWeather(userInput);
   } else {
-    getApi(userInput);
+    getCurrentWeather(userInput);
+    getForecastWeather(userInput);
   }
 
   event.preventDefault();
@@ -68,3 +153,8 @@ $("#todaysDate").text(todaysDate);
 for (let i = 0; i < 5; i++) {
   $("#d" + i).text(dayjs().add(i, "day").format("ddd"));
 }
+
+// lalala.on("click","city-btn",function(event){
+//   var value = event.target.innerText;
+//   currentWeather(value);
+// });
