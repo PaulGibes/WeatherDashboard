d5934e5457877202b9419e5dec27a093d69b3098
paulgibes <paulgibes@gmail.com>
2023-02-22 20:40:34 -0700

    made forecast cards display the next few days

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 var searchHistory = $("#searchHistory");
 var cityHistory = JSON.parse(localStorage.getItem("cities")) || [];
 localStorage.setItem("cities", JSON.stringify(cityHistory));
-var today = dayjs().format("dddd, MMMM D YYYY");
+var todaysDate = dayjs().format("dddd, MMMM D YYYY");
+var today = dayjs().format("ddd");
+var week = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 var requestUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=denver&appid=b18b2ada8a7cf01d1a6c89d2666509ec";
 
@@ -31,7 +33,7 @@ fetch(requestUrl)
   })
   .then(function (data) {});
 
-// dynamically created elements won't have the click event bound to them
+// dynamically created elements won't have the click event bound to them so do this
 $("#searchHistory").on("click", "#clearHistoryButton", function () {
   localStorage.clear();
   searchHistory.empty();
@@ -52,6 +54,23 @@ $("#searchForm").submit(function (event) {
   event.preventDefault();
 });
 
+// forecast = [];
+for (let i = 0; i < 5; i++) {
+  // forecast.push(dayjs().add(i, "day").format("ddd"));
+  $("#d" + i).text(dayjs().add(i, "day").format("ddd"));
+}
+
 seeHistory();
 
-$("#todaysDate").text(today);
+$("#todaysDate").text(todaysDate);
+// $("#d0").text(today);
+// $("#d1").text(todayPlus1);
+// $("#d2").text(todayPlus2);
+// $("#d3").text(todayPlus3);
+// $("#d4").text(todayPlus4);
+
+// if(loopvariable = arrayLength){
+//   set loopVariable = 0
+// }
+
+// $(".card-img-top").attr("src", "./assets/images/cloud-solid.svg");
